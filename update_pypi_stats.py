7e53fb770dd65b88f1cd0eef11006ca1e57db583
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-07-01 12:11:10 +0100

    Tiding up get release information

--- update_pypi_stats.py
+++ update_pypi_stats.py
@@ -3,9 +3,11 @@ Searches for packages on pypi with SciKit-Surgery in the name, then
 gets some statistics for them
 """
 
+import os.path
 import urllib.request
 import subprocess
 import json
+from datetime import datetime
 
 #packages = os.system('./pypi-simple-search scikit-surgery')
 
@@ -18,46 +20,77 @@ def find_new_pypi_packages(searchname):
                     capture_output=True).stdout
     return packages.decode('utf-8').splitlines()
 
-def init_packages(packages, path = 'libraries/'):
+def add_packages(packages, path = 'libraries/'):
     """
-    Searches through path directory for json files
+    Searches through path directory for marker files
     for each package in list, creates file if not already present
     """
-
     for package in packages:
-        filename = str('libraries/' + package + '.json')
-        try:
-            with open (filename, 'r') as read_file:
-                package_data = json.load(read_file)
+        filename = str('libraries/' + package)
+        if not os.path.isfile(filename):
+            print("Found new package ", package)
+            with open(filename, 'w'):
+                pass
 
-        except(FileNotFoundError):
-            with open (filename, 'w') as write_file:
-                json.dump({ 'name' : package }, write_file)
-
-def get_releases(packages):
+def get_release_information(package_dictionary):
     """
     Queries PyPi to get releases for each package
+    returns the number of releases, first release date, 
+    last release date, last release name
     """
-    for package in packages:
-        print("getting releases for ", package)
-        url = str('https://pypi.org/pypi/' + package + '/json')
-        request = urllib.request.Request(url)
-        response = urllib.request.urlopen(request)
-        data = response.read()
-        values = json.loads(data)
-        releases = values.get('releases')
-        releases_list = list(values.get('releases'))
-        print(str(next(iter(releases))))
-        for release in releases_list:
-            print(release, end = " ")
-            release_date = releases.get(release)[0].get('upload_time')
-            print(release_date)
-            #print (values.get('releases'))
+    releases = package_dictionary.get('releases')
+    releases_list = list(package_dictionary.get('releases'))
+    first_release_date = None
+    last_release_date = None
+    last_release_name = None
+    number_of_releases = len(releases_list)
 
+    for release in releases_list:
+        print(release, end = " ")
+        release_date_string = releases.get(release)[0].get('upload_time')
+        release_date = datetime.fromisoformat(release_date_string)
+        if first_release_date is None:
+            first_release_date = release_date
+        if last_release_date is None:
+            last_release_date = release_date
+
+        if release_date < first_release_date:
+            first_release_date = release_date
+        if release_date > last_release_date:
+            last_release_date = release_date
+            last_release_name = release
+
+
+        #release_date = datetime.strptime(release_date_string, 'iso')
+        print(release_date)
+    return number_of_releases, first_release_date, last_release_date,\
+                   last_release_name
+
+def get_json_from_pypi(package):
+    """
+    gets data on the package from pypi
+    """
+    url = str('https://pypi.org/pypi/' + package + '/json')
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    data = response.read()
+    return json.loads(data)
 
 if __name__ == '__main__':
-    packages = find_new_pypi_packages('scikit-surgery')
-    init_packages(packages)
-    get_releases(packages)
+    new_packages = find_new_pypi_packages('scikit-surgery')
+    #add_packages(new_packages)
+    packages = os.listdir('libraries/')
+    for package in packages:
+        if os.path.isdir(package) or package.endswith(".txt"):
+            packages.remove(package)
+    
+    package_dictionaries = []
+    for package in packages:
+        package_dictionaries.append(get_json_from_pypi(package))
+
+    for dictionary in package_dictionaries:
+        print("getting releases for ", dictionary.get('info').get('name'))
+        print(get_release_information(dictionary))
+
 
    
