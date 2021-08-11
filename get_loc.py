cdb3358840f678ff075fbd692b6f457f60ba1a85
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-08-11 11:11:40 +0100

    Gets loc

--- get_loc.py
+++ get_loc.py
@@ -22,9 +22,23 @@ if __name__ == '__main__':
         print("Counting lines of ", package)
 
         homepage = get_package_information(package, 'home_page')
+        current_dir = os.getcwd()
         if homepage is not None:
+            os.chdir(current_dir)
             shutil.rmtree('/dev/shm/sks_temp_for_cloc', ignore_errors = True)
             subprocess.run(['git',  'clone', homepage, '/dev/shm/sks_temp_for_cloc'])
+
+            os.chdir('/dev/shm/sks_temp_for_cloc')
+            commits = subprocess.run(['git', 'log', '--format="%ct %h"'], 
+                    capture_output=True).stdout
+            for commit in commits.decode('utf-8').splitlines():
+                date = commit.split()[0].replace('"', '')
+                githash = commit.split()[1].replace('"', '')
+                print ('Date:' , date, ' hash:', githash, end='')
+                loc = subprocess.run(['cloc', githash, '--quiet'],
+                    capture_output=True).stdout
+                total = loc.decode('utf-8').replace('-','').split()[-1]
+                print (" Sum = ", total)
             exit()
         else:
             print (package , " has no homepage")
