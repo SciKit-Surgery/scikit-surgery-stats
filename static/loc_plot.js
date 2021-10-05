48372482569ff0315fd1828b31486fe030191342
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-10-05 12:00:07 +0100

    Issue #1 read and plot data OK

--- static/loc_plot.js
+++ static/loc_plot.js
@@ -2,14 +2,17 @@
 make_plot()
 
 function make_plot(){
-	console.log(loc_data);
+	var data_to_plot = [];
+	for (const [key, value] of Object.entries(loc_data)) {
+		data_to_plot.push({ x: value['date'], y: value['loc']});
+	}
 	var canvas = document.getElementById('lines_of_code_plot');
 	var ctx = canvas.getContext('2d');
 	
 	var data = {
 		datasets: [
-			{ //here we need to make sure the data is in date order or we the connecting line is silly
-				data: [{x: '2021-07-15T00:00:00', hash: 789232, y: 2938}, {x: '2021-07-21T12:00:00', anotherhash: 238298, y: 2911}, {x: '2021-08-11T00:00:00', y: 3014} ],
+			{ 
+				data: data_to_plot,
 				showLine: true,
 				pointBackgroundColor: 'rgba(0,0,0,1.0)'
 			}
