8af4d75d8233dba6ca9c6ba7ab77592b66ab7a78
yidilozdemir <yagmuriozdemir96@gmail.com>
2023-06-16 17:34:02 +0100

    working cookiecutter with syntax fix and simpler library check

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -10,14 +10,13 @@ import sys
 
 import requests
 
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
 library_name = "{{ cookiecutter.base_library_name }}"
 
 response = requests.get(
     f"https://pypi.org/search/?q={library_name}&o=", timeout=20
-)  # 20 seconds
+)  # wait 20 seconds
 
-if not re.match(MODULE_REGEX, library_name) or response.status_code != 200:
+if response.status_code != 200:
     print("ERROR: %s is not a valid Python module name!" % library_name)
     sys.exit(1)
