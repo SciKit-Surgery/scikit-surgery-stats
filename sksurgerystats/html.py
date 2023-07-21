67ded0d04fcecc8c76ab404abf4c18e07a546f8e
yidilozdemir <yidilozdemir@users.noreply.github.com>
2023-07-21 00:46:33 +0000

    Deploying to gh-pages from @ SciKit-Surgery/scikit-surgery-stats@e39d143265355ac91b16b013a5225b082a332e92 🚀

--- sksurgerystats/html.py
+++ sksurgerystats/html.py
@@ -4,18 +4,14 @@ import json
 
 
 def load_cache_file(filename):
-    """Loads lines of code data from filename, stripping
+    """Loads lines of code data from filename, stripping 
     var_name from the front
     """
     ret_dict = {}
     try:
-        with open(filename, "r") as filein:
-            file_content = filein.read()
-            # If there is a file but there are no hash entries inside i.e. the first time this functionality is run
-            if len(file_content) == 0:
-                return ret_dict
+        with open(filename, 'r') as filein:
             try:
-                jsontext = file_content.split("=")[1]
+                jsontext = filein.read().split('=')[1]
                 ret_dict = json.loads(jsontext)
             except json.JSONDecodeError:
                 raise json.JSONDecodeError
@@ -26,45 +22,46 @@ def load_cache_file(filename):
 
     return ret_dict
 
-
-def make_html_file(package, jsfile, template_file="templates/loc_plot.html"):
-    """Write Lines of Code information to html files of each library stored under /libraries/"""
-    # create dir if not existing
+def make_html_file(package, jsfile, 
+        template_file = 'templates/loc_plot.html'):
+    """Write Lines of Code information to html files of each library stored under /libraries/
+    """
+    #create dir if not existing
     try:
-        os.mkdir("loc/")
+        os.mkdir('loc/')
     except FileExistsError:
         pass
 
-    with open(template_file, "r") as filein:
+    with open(template_file, 'r') as filein:
         template = filein.read()
 
-    with_title = template.replace("PAGE_TITLE", str(package + " Lines of Code"))
-    with_heading = with_title.replace(
-        "CHART_HEADING", str(package + " Lines of Code vs Date")
-    )
-    with_data = with_heading.replace("PATH_TO_DATA", str("../" + jsfile))
+    with_title = template.replace('PAGE_TITLE', str(package + ' Lines of Code'))
+    with_heading = with_title.replace('CHART_HEADING', str(package + ' Lines of Code vs Date'))
+    with_data = with_heading.replace('PATH_TO_DATA', str('../' + jsfile))
 
-    with open(str("loc/" + package + ".html"), "w") as fileout:
+    with open(str('loc/' + package + '.html'), 'w') as fileout:
         fileout.write(with_data)
 
-
 def write_to_js_file(data, fileout):
-    """Write git hashes and date information to js files of each library stored under /libraries/"""
-    outstring = str("var loc_data = " + json.dumps(data))
-    with open(fileout, "w") as fileout:
+    """Write git hashes and date information to js files of each library stored under /libraries/
+    """
+    outstring = str('var loc_data = ' + json.dumps(data))
+    with open(fileout, 'w') as fileout:
         fileout.write(outstring)
 
 
-def WriteCellWithLinkedImage(fileout, image=None, link=None, alt_text=None):
+def WriteCellWithLinkedImage(fileout, image = None, link = None, 
+                             alt_text = None):
     """
     Write a cell to fileout with image and link
     if image is none it writes an empty cell
     """
-    fileout.write("    <td>\n")
+    fileout.write('    <td>\n')
     if image is not None:
         fileout.write(str('      <a href="' + str(link) + '">\n'))
-        fileout.write(
-            str('        <img src="' + str(image) + '" alt="' + alt_text + '">\n')
-        )
-        fileout.write(str("      </a>\n"))
-    fileout.write("    </td>\n")
+        fileout.write(str('        <img src="' + str(image) + '" alt="' +
+                          alt_text + '">\n'))
+        fileout.write(str('      </a>\n'))
+    fileout.write('    </td>\n')
+
+
